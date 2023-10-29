30ae931fd5b363c019a8aa6fc318fb424da1e321
makano <makanobush@gmail.com>
2023-10-29 11:05:20 +0300

    port in config

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,19 @@
 import { error, Router } from "itty-router";
+import { readFileSync, writeFileSync, existsSync } from "fs";
 import { middleWare } from "./modules/middleware/middleware";
 import { serve } from "./modules/serving/server.bun";
 
 
 async function startServer(){
+	let port = 3000;
+	
+	if(existsSync('./rayous.json')){
+		const json = JSON.parse(readFileSync('./rayous.json').toString());
+		if(json.port) port = parseInt(json.port) || 3000;
+	} else {
+		writeFileSync('./rayous.json', '{}');
+	}
+	
 
 	const router = Router();
 
@@ -11,10 +21,10 @@ async function startServer(){
 
 	router.all("*", (req: Request) => serve(req));
 
-	console.log('localhost:3000');
+	console.log('localhost:'+port);
 	// @ts-ignore
 	const server = Bun.serve({
-		port: 3000,
+		port,
 		fetch: (request: Request) => router.handle(request).catch(error),
 		websocket: sockets
 	});
