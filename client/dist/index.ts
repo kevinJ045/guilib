bb580627c91fee2ec183e6b6029fbc39a29ddda4
makano <makanobush@gmail.com>
2023-12-05 00:24:00 +0300

    dist: component scripts

--- client/dist/index.ts
+++ client/dist/index.ts
@@ -5,7 +5,110 @@ import * as svg from "../svg";
 import Dom from "../utils/dom";
 import { WidgetList } from "../widgets/_ghost/WidgetProps";
 
+function makeComponent(componentString: string){
+	if(!componentString) return "";
+	return `(() => {
+const { ${Object.keys(widgets)} } = window.Rayous?.widgets || {};
+const { ${Object.keys(extra)} } = window.Rayous?.extra || {};
+${componentString.trim().startsWith('return') ? '' : 'return '}${componentString.trim()}
+})`;
+}
+
+function callComponentClass(componentFunction: () => any, script: HTMLScriptElement){
+
+	let componentClass = componentFunction();
+	if(!componentClass) return;
+	if(typeof componentClass == "function" && !componentClass.buildFor) componentClass = componentClass();
+	if(!componentClass.buildFor) return;
+
+
+	// @ts-ignore
+	const props = {...window.RayousBuildProps};
+
+	const scriptProps = script.attributes.getNamedItem('props');
+	if(scriptProps?.textContent){
+		try{
+			let p = eval('('+scriptProps?.textContent+')');
+			if(typeof p == "object") for(let i in p) props[i] = p[i];
+		} catch(e){
+			console.error(e);
+		}
+	}
+	
+	const component = componentClass.buildFor(null, props);
+	if(!component) return;
+	if(!component.to) return;
+	const newElement = document.createElement('div');
+	script.parentNode!.insertBefore(newElement, script.nextSibling);
+	component.to(newElement);
+}
+
+function buildScriptComponent(script: HTMLScriptElement, scriptContent: string){
+	const componentFunction = eval(makeComponent(scriptContent));
+
+	if(!componentFunction) return;
+	if(typeof componentFunction !== "function") return;
+	
+	callComponentClass(componentFunction, script);
+}
+
 class Rayous {
+
+	constructor(){
+		const rayous = this;
+		window.addEventListener('load', () => {
+			// @ts-ignore
+			const componentScripts: HTMLScriptElement[] = document.querySelectorAll('script[type="rayous-component"]');
+
+			// Iterate through each script tag and create an instance of the corresponding class
+			componentScripts.forEach((script) => {
+				const scriptContent: string = (script.textContent || script.innerText) || "";
+				if(scriptContent){
+					buildScriptComponent(script, scriptContent);
+				} else {
+					if(script.hasAttribute('src')){
+						const src = script.getAttribute('src')!;
+						const componentName = script.getAttribute('componentName');
+
+						function findScript(){
+							const _script = document.createElement('script');
+							_script.src = src;
+							_script.onload = () => {
+								callComponentClass(() => rayous.components[componentName!], script);
+								delete rayous.components[componentName!];
+								_script.remove();
+							}
+							document.head.appendChild(_script);
+						}
+
+						function fetchScript(){
+							fetch(src)
+							.then(r => r.text())
+							.then(text => buildScriptComponent(script, text))
+							.catch(e => console.error(e));
+						}
+
+						if(componentName){
+							findScript();
+						} else {
+							try{
+								fetchScript();
+							} catch(e){
+								findScript();
+							}
+						}
+						
+					}
+				}
+			});
+		});
+	}
+
+	components: Record<string, any> = {};
+	component(component: () => any){
+		this.components[component.name] = component;
+	}
+
 	extra = extra;
 	html = html;
 	widgets = widgets;
--- docs/views/Standalone.md
+++ docs/views/Standalone.md
@@ -20,3 +20,72 @@ Once you include that js file you can just:
 	Rayous.dom('.widget').remove();
 </script>
 ```
+
+### Rayous Script Components
+Rayous Standalone Script Components are script tags that can hold a component class, for example:
+```html
+<script type="rayous-component">
+	class extends Component{
+
+		build(){
+			return new Widget({
+				children: [ new Text('hi') ]
+			})
+		}
+
+	}
+</script>
+```
+
+#### Custom properties
+Custom buildProps for `rayous-component` scripts.
+```html
+<script type="rayous-component" props="{ prop: 'value' }">
+	class extends Component{
+
+		build({ prop }){
+			return new Widget({
+				children: [ new Text(prop) ] // 'value'
+			})
+		}
+
+	}
+</script>
+```
+
+### External components
+There are two types of external components,
+In the simpler version, all you have to do is:
+```html
+<script type="rayous-component" src="/path/to/component.js"></script>
+```
+and in the js:
+```ts
+class extends Component {
+	build({ prop }){
+		return new Widget({
+			children: [ new Text(prop) ] // 'value'
+		})
+	}
+}
+```
+you should put the class code before anything else, if the component class is not declared
+right at the start, errors might happen. this one uses the `fetch` api, and it might not always work as expected.
+
+The other one is more complex, you have to actually define the component inside the script, and put the name inside the html:
+```html
+<script type="rayous-component" src="/path/to/component.js" componentName="ExampleComponent"></script>
+```
+In the JS, all you have to do is:
+```
+const { Component, Widget, Text } = Rayous.widgets; 
+
+Rayous.component(class ExampleComponent extends Component {
+	build({ prop }){
+		return new Widget({
+			children: [ new Text(prop) ] // 'value'
+		})
+	}
+});
+```
+It is important to name the class the same name you put in the `componentName` attribute. In this one, fetch is not used, and you can use anything you like inside it, as long as it calls the `Rayous.component` with an extension class for `Component`, and a name of `componentName`.
