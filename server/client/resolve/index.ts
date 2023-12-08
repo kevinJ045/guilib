5589452c1b7f37dd9d1c53a9a54c264c38c9436f
makano <makanobush@gmail.com>
2023-12-08 13:42:01 +0300

    Change will not come if we wait for some other person or some other time. We are the ones we've been waiting for. We are the change that we seek.

--- server/client/resolve/index.ts
+++ server/client/resolve/index.ts
@@ -3,15 +3,21 @@ function getAllPaths(){
 	return (window as Record<string, any>).all_possible_paths as {pathname: string}[];
 }
 
+function pathToRegex(path: string): RegExp {
+	if (path === '/') return /\/$/;
+	return new RegExp(`${path.replace(/\/$/, '').replace(/\:([\w]+)/g, '(.+)')}`);
+}
+
 export function getRootPath(){
 	let rootPath = "";
 	let current = location.pathname;
 	if(current.endsWith('/') && current !== '/') current = current.replace(/\/$/, '');
 	const allPaths = getAllPaths();
-	const foundPath = allPaths.filter(path => path.pathname.length).find(path => current.endsWith(path.pathname));
-	
+	const foundPath = allPaths.filter(path => path.pathname.length)
+	.find(path => path.pathname == '/' ? current == '/' : current.match(pathToRegex(path.pathname)));
+
 	if(foundPath){
-		let path = current.replace(foundPath.pathname, '');
+		let path = current.replace(pathToRegex(foundPath.pathname), '');
 		rootPath = path;
 	}
 
